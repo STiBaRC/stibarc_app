2461076dde936df3eeff60e726e4f79656ddff86
Joshua Herron <herronjo110@live.com>
2018-03-08 15:39:44 -0500

    Update login.js

--- www/js/login.js
+++ www/js/login.js
@@ -1,4 +1,4 @@
-﻿var login = function () {
+var login = function () {
     document.getElementById("blank").style.display = "none";
     document.getElementById("badnamepass").style.display = "none";
     var username = document.getElementById("username").value;
