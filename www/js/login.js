c5b2f3b3dd75e2578bcd82388a68826ba073a2c3
Joshua Herron <herronjo110@live.com>
2018-03-08 11:42:34 -0500

    Update login.js

--- www/js/login.js
+++ www/js/login.js
@@ -13,6 +13,7 @@
         if (sess != "Invalid username or password\n") {
             //document.cookie = "sess=" + sess;
 			window.localStorage.setItem("sess", sess.split("\n")[0]);
+			window.localStorage.setItem("username", username);
 			location.href = "index.html"
         } else {
             document.getElementById("badnamepass").style.display = "";
