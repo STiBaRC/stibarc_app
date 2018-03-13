5908452c407e9528b14d75778160fa4059f04c98
Joshua Herron <herronjo110@live.com>
2018-03-13 18:07:03 -0400

    Update login.js

--- www/js/login.js
+++ www/js/login.js
@@ -5,9 +5,9 @@ var login = function () {
     var password = document.getElementById("password").value;
     if (username != undefined && username != "" && password != undefined && password != "") {
         var xmlHttp = new XMLHttpRequest();
-		var appid = window.localStorage.getItem("appID");
-		if (appid == "" || appid == null || appid == undefined) appid = "none";
-        xmlHttp.open("GET", "https://api.stibarc.gq/createsess.sjs?username=" + username + "&password=" + password + "&appid=" + appid , false);
+	//var appid = window.localStorage.getItem("appID");
+	//if (appid == "" || appid == null || appid == undefined) appid = "none";
+        xmlHttp.open("GET", "https://api.stibarc.gq/createsess.sjs?username=" + username + "&password=" + password, false);
         xmlHttp.send(null);
         sess = xmlHttp.responseText;
         if (sess != "Invalid username or password\n") {
