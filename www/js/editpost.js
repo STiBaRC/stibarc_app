9382d044addc9353e09019bee8c0feb5397536ab
Joshua Herron <herronjo110@live.com>
2018-03-13 21:19:28 -0400

    Update editpost.js

--- www/js/editpost.js
+++ www/js/editpost.js
@@ -1,3 +1,9 @@
+var senderr = function(err) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("post", "https://api.stibarc.gq/senderror.sjs");
+  xmlHttp.send("error="+err);
+}
+
 function getAllUrlParams(url) {
     var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
     var obj = {};
@@ -54,31 +60,39 @@ var toJSON = function (cookie) {
 }
 
 var post = function () {
-	var content = document.getElementById("content").value;
-	var title = document.getElementById("title").value;
-	var sess = window.localStorage.getItem("sess");
-	var id = getAllUrlParams().id;
-	if (content != "" && content != undefined && title != "" && title != undefined) {
-		var thing = new XMLHttpRequest();
-		thing.open("POST", "https://api.stibarc.gq/editpost.sjs", false);
-		thing.send("sess="+sess+"&id="+id+"&title="+encodeURIComponent(title)+"&content="+encodeURIComponent(content).replace(/%0A/g, "%0D%0A"));
-		location.href = "post.html?id=" + id;
-		document.getElementById("content").value = "";
-		document.getElementById("title").value = "";
+	try {
+		var content = document.getElementById("content").value;
+		var title = document.getElementById("title").value;
+		var sess = window.localStorage.getItem("sess");
+		var id = getAllUrlParams().id;
+		if (content != "" && content != undefined && title != "" && title != undefined) {
+			var thing = new XMLHttpRequest();
+			thing.open("POST", "https://api.stibarc.gq/editpost.sjs", false);
+			thing.send("sess="+sess+"&id="+id+"&title="+encodeURIComponent(title)+"&content="+encodeURIComponent(content).replace(/%0A/g, "%0D%0A"));
+			location.href = "post.html?id=" + id;
+			document.getElementById("content").value = "";
+			document.getElementById("title").value = "";
+		}
+	} catch(err) {
+		senderr(err);
 	}
 }
 
 window.onload = function () {
-	var id = getAllUrlParams().id;
-	if (id != "" && id != undefined) {
-		var thing = new XMLHttpRequest();
-		thing.open("GET", "https://api.stibarc.gq/getpost.sjs?id="+id, false);
-		thing.send(null);
-		var tmp = JSON.parse(thing.responseText);
-		document.getElementById("title").value = tmp['title'];
-		document.getElementById("content").value = tmp['content'];
-		document.getElementById("send").onclick = function (evt) {
-			post();
+	try {
+		var id = getAllUrlParams().id;
+		if (id != "" && id != undefined) {
+			var thing = new XMLHttpRequest();
+			thing.open("GET", "https://api.stibarc.gq/getpost.sjs?id="+id, false);
+			thing.send(null);
+			var tmp = JSON.parse(thing.responseText);
+			document.getElementById("title").value = tmp['title'];
+			document.getElementById("content").value = tmp['content'];
+			document.getElementById("send").onclick = function (evt) {
+				post();
+			}
 		}
+	} catch(err) {
+		senderr(err);
 	}
 }
