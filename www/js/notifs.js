a8506d5285ff35c3d132f35fa901d60eee838081
Joshua Herron <herronjo110@live.com>
2018-04-06 20:26:24 -0400

    Update notifs.js

--- www/js/notifs.js
+++ www/js/notifs.js
@@ -12,15 +12,44 @@ var checkNotifs = function() {
 		}
 		text = text.concat(tmp[tmp.length-3]);
 		window.localStorage.setItem("lastNotifID", tmp[0]);
-        cordova.plugins.notification.local.schedule({
-            title: "New post",
-            message: text,
-            icon: "https://stibarc.gq/icon.png"
-        });
+        	cordova.plugins.notification.local.schedule({
+            		title: "New post",
+            		message: text,
+            		icon: "file://android_asset/icon.png"
+        	});
 		cordova.plugins.notification.local.on("click", function (notification) {
 			var postID = tmp[tmp.length-2];
 			window.location.assign("post.html?id="+postID);
 		});
+	}
+}
+
+var checkNotifsUser = function(user) {
+	var perms = window.localStorage.getItem("notifs");
+	if (perms == "granted") {
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.open("post", "https://api.stibarc.gq/getusernotifs.sjs", false);
+		xmlHttp.send("id="+user);
+		var tmp = xmlHttp.responseText.split("\n");
+		var lastID = window.localStorage.getItem("lastUserNotifID");
+		if (lastID == "" || lastID == undefined || lastID == null) {lastID = -1;}
+		if (tmp[0].concat(tmp[tmp.length-2]) != lastID) {
+			var text = "";
+			for (var i = 2; i < tmp.length-3; i++) {
+				text = text.concat(tmp[i]+"\n");
+			}
+			text = text.concat(tmp[tmp.length-3]);
+			window.localStorage.setItem("lastUserNotifID", tmp[0].concat(tmp[tmp.length-2]));
+			cordova.plugins.notification.local.schedule({
+				title: tmp[1],
+				message: text,
+				icon: "file://android_asset/icon.png"
+			});
+			cordova.plugins.notification.local.on("click", function (notification) {
+				var postID = tmp[tmp.length-2];
+				window.location.assign("post.html?id="+postID);
+			});
+		}
 	}
 }
 
@@ -43,7 +72,14 @@ var checkNotifs = function() {
 	}
 }*/
 
+var doCheck = function() {
+	checkNotifs();
+	if (window.localStorage.getItem("username") != null || window.localStorage.getItem("username") != undefined || window.localStorage.getItem("username") != "") {
+		checkNotifsUser(window.localStorage.getItem("username"));
+	}
+}
+
 var startNotifs = function() {
 	//setupNotifs();
-	setInterval(checkNotifs, 500);
+	setInterval(doCheck, 500);
 }
